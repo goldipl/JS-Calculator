22bab73416c1c0eeed90878d344872d82fb26280
goldipl <mgodlewski92@gmail.com>
2021-05-03 21:47:26 +0200

    add handling dot & AC button

--- js/calcScript.js
+++ js/calcScript.js
@@ -9,6 +9,8 @@ const btnNr8 = document.querySelector(".nr8");
 const btnNr9 = document.querySelector(".nr9");
 const btnNr0 = document.querySelector(".nr0");
 const btnNumbers = [btnNr1, btnNr2, btnNr3, btnNr4, btnNr5, btnNr6, btnNr7, btnNr8, btnNr9, btnNr0];
+const btnAC = document.querySelector(".btn.ac");
+const btnDot = document.querySelector(".btn.dot");
 const calcDisplay = document.querySelector("input");
 
 //getting number to display
@@ -17,4 +19,19 @@ btnNumbers.forEach(button => {
         btnNumber = button.dataset.num;
         calcDisplay.value += btnNumber;
     });
+});
+
+//AC Button
+btnAC.addEventListener("click", () => {
+    calcDisplay.value = '';
+});
+
+//dot Button
+btnDot.addEventListener("click", () => {
+    if (!calcDisplay.value.includes('.')) {
+        dotValue = '.';
+        calcDisplay.value += dotValue;
+    } else {
+        return;
+    }
 });
