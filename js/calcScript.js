bf34cb7ec327cb6e32a6fbfdeb5b9a0ef028f6ee
goldipl <mgodlewski92@gmail.com>
2021-05-03 20:24:57 +0200

    1st commit

---
+++ js/calcScript.js
@@ -0,0 +1,11 @@
+const btnNr1 = document.querySelector(".nr1");
+const btnNr2 = document.querySelector(".nr2");
+const btnNr3 = document.querySelector(".nr3");
+const btnNr4 = document.querySelector(".nr4");
+const btnNr5 = document.querySelector(".nr5");
+const btnNr6 = document.querySelector(".nr6");
+const btnNr7 = document.querySelector(".nr7");
+const btnNr8 = document.querySelector(".nr8");
+const btnNr9 = document.querySelector(".nr9");
+const btnNr0 = document.querySelector(".nr0");
+const btnNumbers = [btnNr1, btnNr2, btnNr3, btnNr4, btnNr5, btnNr6, btnNr7, btnNr8, btnNr9, btnNr0];
